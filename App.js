4e94d4129543befab61433a7fc7c8e8585f1dbe5
ankittkamal <officiallyankit999@gmail.com>
2023-04-04 11:18:31 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from react 🚀"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
